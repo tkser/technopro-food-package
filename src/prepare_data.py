07b9155715a0dc19222ef392ac02bca4588d6af2
tkser <tkser.edu@gmail.com>
2023-08-29 09:28:33 +0000

    add: prepare data cmd

---
+++ src/prepare_data.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import zipfile
+
+
+def exract_zip_file(zip_file_path, extract_path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+
+def remove_macosx_dir(dir_path):
+    macosx_dir_path = os.path.join(dir_path, '__MACOSX')
+    if os.path.exists(macosx_dir_path):
+        shutil.rmtree(macosx_dir_path, ignore_errors=True)
+
+
+def main():
+    input_data_dir = os.path.join(os.path.dirname(__file__), './data/input')
+
+    train_zip_path = os.path.join(input_data_dir, 'train.zip')
+    test_zip_path = os.path.join(input_data_dir, 'test.zip')
+
+    if not os.path.exists(train_zip_path) or not os.path.exists(test_zip_path):
+        print("Error: train.zip and/or test.zip not found in the specified directory.")
+        return
+
+    train_extract_path = os.path.join(input_data_dir, 'train')
+    test_extract_path = os.path.join(input_data_dir, 'test')
+
+    if not os.path.exists(train_extract_path):
+        exract_zip_file(train_zip_path, input_data_dir)
+        print("Extracted train.zip to {}".format(train_extract_path))
+    else:
+        print("train.zip already extracted to {}".format(train_extract_path))
+    
+    if not os.path.exists(test_extract_path):
+        exract_zip_file(test_zip_path, input_data_dir)
+        print("Extracted test.zip to {}".format(test_extract_path))
+    else:
+        print("test.zip already extracted to {}".format(test_extract_path))
+    
+    remove_macosx_dir(input_data_dir)
+    
+
+if __name__ == '__main__':
+    main()
