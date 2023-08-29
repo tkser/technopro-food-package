3c7b670ad0c029db60afa1b6bedcf4778080aebb
tkser <tkser.edu@gmail.com>
2023-08-29 09:45:18 +0000

    add: set_seed method

---
+++ src/utils/set_seed.py
@@ -0,0 +1,12 @@
+import torch
+import random
+import numpy as np
+
+
+def set_seed(seed = 0):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
